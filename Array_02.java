c6063bf4a2096c82989ef5eaa51e06e9aee66337
ShanmukhaKrishnaChaitanya <151119324+ShanmukhaKrishnaChaitanya@users.noreply.github.com>
2025-02-06 13:30:19 +0530

    Add files via upload

---
+++ Array_02.java
@@ -0,0 +1,9 @@
+public class Array_02 {
+    public static void main(String[] args) {
+        String[] names = {"Krishna", "Vasudeva", "Jagannadha", "Purushothama", "Parandhama"};
+        for(int i=0; i<names.length; i++)
+        {
+            System.out.print(names[i] + " ");
+        }
+    }
+}
